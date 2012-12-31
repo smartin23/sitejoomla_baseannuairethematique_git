687ca2e5418d3ea9a131a068050f94abc94cd142
Stephane Martin <stephane.martin23@gmail.com>
2012-12-31 12:11:22 +0100

    Mise en commun dans baseannuaire

--- mklinks.bat
+++ mklinks.bat
@@ -1,10 +1,13 @@
 d:
 cd \wamp21e\www\sitejoomla_baseannuairethematique\components
-MKLINK /D com_sobipro D:\wamp21e\www\sitejoomla_baseannuairethematique_git\components\com_sobipro
+rmdir com_sobipro
+MKLINK /D com_sobipro D:\wamp21e\www\sitejoomla_baseannuaire_git\components\com_sobipro
 pause
 cd \wamp21e\www\sitejoomla_baseannuairethematique\modules
-MKLINK /D mod_jmaps D:\wamp21e\www\sitejoomla_baseannuairethematique_git\modules\mod_jmaps
+rmdir mod_jmaps
+MKLINK /D mod_jmaps D:\wamp21e\www\sitejoomla_baseannuaire_git\modules\mod_jmaps
 pause
 cd \wamp21e\www\sitejoomla_baseannuairethematique\templates
+rmdir baseannuairethematique
 MKLINK /D baseannuairethematique D:\wamp21e\www\sitejoomla_baseannuairethematique_git\templates\baseannuairethematique
 pause
